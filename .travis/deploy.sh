90e7621ad393cd8271bb8ab64cf79d4d731206e7
dz0ny <dz0ny@ubuntu.si>
2017-02-23 19:58:21 +0100

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+echo -e "Host $APP_HOSTNAME\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 eval "$(ssh-agent -s)" #start the ssh agent
-echo "$FUNNY_CAT" > .travis/deploy_key.pem
+openssl aes-256-cbc -K $encrypted_8b6bbaa2f436_key -iv $encrypted_8b6bbaa2f436_iv -in .travis/key.enc -out .travis/deploy_key.pem -d
 chmod 600 .travis/deploy_key.pem # this key should have push access
 ssh-add .travis/deploy_key.pem
+rm -rf .travis/deploy_key.pem
 git remote add dokku dokku@$APP_HOSTNAME:$APP
 git push --force dokku HEAD:master
