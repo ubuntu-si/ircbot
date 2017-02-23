ff25fd050f92e3456ced6de71b54198613380f60
Janez Troha <dz0ny@users.noreply.github.com>
2017-02-23 19:13:36 +0100

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,7 @@
-
+#!/bin/bash
+eval "$(ssh-agent -s)" #start the ssh agent
+echo "$FUNNY_CAT" > .travis/deploy_key.pem
+chmod 600 .travis/deploy_key.pem # this key should have push access
+ssh-add .travis/deploy_key.pem
 git remote add dokku dokku@$APP_HOSTNAME:$APP
 git push --force dokku HEAD:master
